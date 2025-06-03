b35fbe95a99e415731bb64af491018db60e8dc20
joseimdev <joseimdev@gmail.com>
2025-06-03 14:14:04 +0900

    [fix] Wordle 로비에서 Continue Game 버튼 조건부 렌더링 수정

--- components/page/gameLobby/WodleLobby.tsx
+++ components/page/gameLobby/WodleLobby.tsx
@@ -1,14 +1,16 @@
 import { useAd } from "@/contexts/AdContext/AdContext";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 import { useRouter } from "expo-router";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { Alert, Text, TouchableOpacity, View } from "react-native";
 
 export default function WordleLobby() {
   const router = useRouter();
 
   const { showStartAd, isStartAdLoaded, isStartAdClosed } = useAd();
+  const [isSavedGame, setIsSavedGame] = React.useState(false);
 
   const handleStart = async () => {
     if (isStartAdLoaded) {
@@ -23,6 +25,28 @@ export default function WordleLobby() {
       router.push("/(games)/wordle");
     }
   }, [isStartAdClosed]);
+
+  useFocusEffect(
+    useCallback(() => {
+      const checkSavedGame = async () => {
+        try {
+          const savedGame = await AsyncStorage.getItem("wordleSavedGame");
+          if (savedGame) {
+            setIsSavedGame(true);
+          } else {
+            setIsSavedGame(false); // 없을 경우 초기화도 필요하다면
+          }
+        } catch (e) {
+          Alert.alert(
+            "Error occurred while checking saved game",
+            "Please try again."
+          );
+        }
+      };
+
+      checkSavedGame();
+    }, [])
+  );
 
   const handleNewGame = async () => {
     try {
@@ -68,15 +92,18 @@ export default function WordleLobby() {
 
       {/* 글자 수 선택 버튼 */}
       <View className="w-full h-[59%] flex items-center py-8">
-        <TouchableOpacity
-          className="w-[50%] h-[56px] border-[2px] border-[#C5723F] rounded-full flex justify-center items-center mb-5"
-          activeOpacity={0.85}
-          onPress={() => handleStart()}
-        >
-          <Text className="text-[20px] font-[nunito] font-bold text-white">
-            Continue Game
-          </Text>
-        </TouchableOpacity>
+        {isSavedGame && (
+          <TouchableOpacity
+            className="w-[50%] h-[56px] border-[2px] border-[#C5723F] rounded-full flex justify-center items-center mb-5"
+            activeOpacity={0.85}
+            onPress={() => handleStart()}
+          >
+            <Text className="text-[20px] font-[nunito] font-bold text-white">
+              Continue Game
+            </Text>
+          </TouchableOpacity>
+        )}
+
         <TouchableOpacity
           className="w-[50%] h-[56px] bg-[#C5723F] rounded-full flex justify-center items-center mb-5"
           activeOpacity={0.85}
